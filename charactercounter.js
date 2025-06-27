5b7a31f1e8027d01400c2aefec8d519f62f74224
David Lazic <lazic.ddavid@gmail.com>
2025-06-27 22:36:14 +0200

    added addEventListener

--- charactercounter.js
+++ charactercounter.js
@@ -2,3 +2,9 @@ const textarea = document.getElementById("textarea");
 const total = document.getElementById("total");
 const remaining = document.getElementById("remaining");
 const maxLength = textarea.maxLength;
+
+textarea.addEventListener("input", () => {
+  const currentLength = textarea.value.length;
+  total.textContent = currentLength;
+  remaining.textContent = maxLength - currentLength;
+});
